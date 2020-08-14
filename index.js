af77325a1e010e5f60a4b9480f48b732bcf86946
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-14 17:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1, num2;
+function multiply() {
+  return 31 * 2;
+}
+multiply();
+
+function random() {
+return random = Math.floor(Math.random());
+}
+
+
+  let mod = 28 % 6;
